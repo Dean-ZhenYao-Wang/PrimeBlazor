1f46904c24495a3d2ffe0237ab947c584401bc81
王振耀 <137473325@qq.com>
2023-12-25 20:12:40 +0800

    启动ListBox组件

---
+++ PrimeBlazor/PrimeBlazorServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeBlazor
+{
+    public static class PrimeBlazorServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPrimeBalzor(this IServiceCollection services)
+        {
+            ArgumentNullException.ThrowIfNull(services, "services");
+            //services.AddSingleton<ObjectUtilsJsInterop>();
+            return services;
+        }
+    }
+}
