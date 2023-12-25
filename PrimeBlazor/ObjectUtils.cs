744c44bde21d81190944fdaf25ae183d16044634
王振耀 <137473325@qq.com>
2023-12-25 21:16:34 +0800

    筛选列表框

---
+++ PrimeBlazor/ObjectUtils.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeBlazor
+{
+    public static class ObjectUtils
+    {
+        public static string resolveFieldStringData<TEntity>(TEntity data,string field)
+        {
+            if (data != null && !string.IsNullOrWhiteSpace(field))
+            {
+                
+                if (!field.Contains("."))
+                {
+                    var propertyInfo = data.GetType().GetProperty(field);
+                    return propertyInfo.GetValue(data).ToString();
+                }
+                else
+                {
+                    var fields = field.Split(".");
+                    object valueData = data;
+                    for(int i=0,len=fields.Length; i<len; ++i)
+                    {
+                        var propertyInfo = data.GetType().GetProperty(fields[i]);
+                        valueData = propertyInfo.GetValue(valueData);
+                    }
+                    return valueData.ToString();
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
