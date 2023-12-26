96488572d7187825fb91785df0e818cc4399d9c0
王振耀 <137473325@qq.com>
2023-12-26 10:26:45 +0800

    实现的Textarea组件

---
+++ PrimeBlazor/Textarea.razor.js
@@ -0,0 +1,12 @@
+﻿export function resize(element) {
+    element.style.height = '';
+    element.style.height = element.scrollHeight + 'px';
+
+    if (parseFloat(element.style.height) >= parseFloat(element.style.maxHeight)) {
+        element.style.overflowY = "scroll";
+        element.style.height = element.style.maxHeight;
+    }
+    else {
+        element.style.overflow = "hidden";
+    }
+}
