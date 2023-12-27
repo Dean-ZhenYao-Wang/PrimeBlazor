1d96d40456dcfe4dd523a987b39956ef7eaa359f
王振耀 <137473325@qq.com>
2023-12-27 10:28:23 +0800

    改进了带有动态值的复选框演示

--- PrimeBlazorDoc/Models/Theme.cs
+++ PrimeBlazorDoc/Models/Theme.cs
@@ -2,17 +2,17 @@
 {
     public class Theme
     {
-        public string Brand { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
         public string Key { get; set; } = string.Empty;
         public override bool Equals(object? obj)
         {
             if(obj==null||GetType() != obj.GetType()) return false;
             Theme other=(Theme)obj;
-            return Brand.Equals(other.Brand) && Key.Equals(other.Key);
+            return Name.Equals(other.Name) && Key.Equals(other.Key);
         }
         public override int GetHashCode()
         {
-            return HashCode.Combine(Brand, Key);
+            return HashCode.Combine(Name, Key);
         }
     }
 }
