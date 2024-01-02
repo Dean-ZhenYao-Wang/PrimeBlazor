fa73178659460912c1868a33aa71b69406b1252f
王振耀 <137473325@qq.com>
2024-01-02 13:07:16 +0800

    启动自动完成组件

---
+++ PrimeBlazor/AutoComplete.razor.js
@@ -0,0 +1,27 @@
+﻿var outsideClickListener = [];
+export function bindOutsideClickListener(id, overlayVisible, overlay, multiple,input) {
+    if (!outsideClickListener.some(o => o.id === id)) {
+        let outsideClick =async (event) => {
+            if (overlayVisible && overlay && isOutsideClicked(event,multiple,input)) {
+                await dotNetHelper.invokeMethodAsync('hideOverlay');
+            }
+        };
+        outsideClickListener.push({ id: id, o: outsideClick })
+        document.addEventListener('click', outsideClick);
+    }
+}
+export function isOutsideClicked(event, multiple, input) {
+    if (multiple) {
+        return !overlay.contains(event.target) && event.target !== input;
+    }
+    else {
+        return !overlay.contains(event.target) && event.target !== input;
+    }
+}
+export function unbindOutsideClickListener(id) {
+    if (outsideClickListener.some(o => o.id === id)) {
+        let { outsideClick } = outsideClickListener.find(o => o.id === id);
+        document.removeEventListener('click', outsideClick);
+        outsideClickListener = outsideClickListener.filter(o => o.id !== id);
+    }
+}
---
+++ PrimeBlazorDoc/Models/Countries.cs
@@ -0,0 +1,8 @@
+﻿namespace PrimeBlazorDoc.Models
+{
+    public class Countries
+    {
+        public string name { get; set; }
+        public string code { get; set; }
+    }
+}
