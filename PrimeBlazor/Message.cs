26006a82077b68061d41cb30785faaea9fffbddc
王振耀 <wsbp2019@163.com>
2024-01-08 09:16:28 +0800

    Initiated Toast

---
+++ PrimeBlazor/Message.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeBlazor
+{
+    public partial class Message
+    {
+        [Parameter]
+        public string id { get; set; } = Guid.NewGuid().ToString();
+        [Parameter]
+        public string summary { get; set; }
+        [Parameter]
+        public string detail { get; set; }
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || GetType() != obj.GetType()) return false;
+            Message other = (Message)obj;
+            return id.Equals(other.id);
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(id);
+        }
+    }
+}
---
+++ PrimeBlazor/ToastEventBus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeBlazor
+{
+    public class ToastEventBus
+    {
+        private static Dictionary<string, List<Action<Message>>> listeners = new Dictionary<string, List<Action<Message>>>();
+        public static void on(string message, Action<Message> listener)
+        {
+            if (!listeners.ContainsKey(message))
+            {
+                listeners.Add(message, new List<Action<Message>>());
+            }
+            listeners[message].Add(listener);
+        }
+        public static void add(Message message)
+        {
+            if (listeners.ContainsKey("add"))
+                listeners["add"].ForEach(m => m.Invoke(message));
+        }
+    }
+}
