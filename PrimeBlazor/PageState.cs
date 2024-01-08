bb00dc78d249eac9ec8ce5904d29fdac21ab8ffb
王振耀 <wsbp2019@163.com>
2024-01-08 18:31:29 +0800

    Paginator added

---
+++ PrimeBlazor/PageState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeBlazor
+{
+    public class PageState
+    {
+        public int first { get; set; }
+        public int rows { get; set; }
+        public int page { get; set; }
+        public int pageCount { get; set; }
+    }
+}
