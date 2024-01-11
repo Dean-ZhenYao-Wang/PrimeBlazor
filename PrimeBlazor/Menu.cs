018f132c7bdd29a4fb5ee0d62acbb2f0d8505781
王振耀 <wsbp2019@163.com>
2024-01-11 10:57:28 +0800

    Set version

--- PrimeBlazor/Menu.cs
+++ PrimeBlazor/Menu.cs
@@ -7,11 +7,15 @@ using System.Threading.Tasks;
 
 namespace PrimeBlazor
 {
-    public class Menu
+    public partial class Menu
     {
         public string? url { get; set; }
         public string label { get; set; } = string.Empty;
+        public List<Menu> items { get; set; }=new List<Menu>();
         public string? icon { get; set; }
+        public string? CssClass { get; set; } = null;
+        public string? style { get; set; } = null;
+        public string? target { get; set; } = null;
         public Action<CommandEventArgs>? command { get; set; }
     }
 }
---
+++ PrimeBlazor/Menu.razor.js
@@ -0,0 +1,118 @@
+﻿export default class Menu {
+    static dotNetHelperArray = [];
+    static targetArray = [];
+    static outsideClickListener = [];
+    static resizeListener = [];
+    static OnAfterRenderAsync(dotNetHelper, commponentId) {
+        Menu.dotNetHelperArray.push({ id: commponentId, dotNetHelper: dotNetHelper });
+    }
+    static async toggle(event, commponentId) {
+        let { dotNetHelper } = Menu.dotNetHelperArray.find(m => m.id === commponentId);
+        if (await dotNetHelper.invokeMethodAsync("getVisible")) {
+            await Menu.hide(dotNetHelper);
+        } else {
+            await Menu.show(event, dotNetHelper);
+        }
+    }
+    static async show(event, dotNetHelper) {
+        await dotNetHelper.invokeMethodAsync("show");
+        if (Menu.targetArray.some(m => m.id === commponentId)) {
+            let targetObject = Menu.targetArray.find(m => m.id === commponentId);
+            targetObject.target = event.target;
+        } else {
+            Menu.targetArray.push({ id: commponentId, target: event.target });
+        }
+    }
+    static async hide(dotNetHelper) {
+        await dotNetHelper.invokeMethodAsync("hide");
+    }
+    static async onEnter(commponentId) {
+        let { dotNetHelper } = Menu.dotNetHelperArray.find(m => m.id === commponentId);
+        let container = await dotNetHelper.invokeMethodAsync("getContainer")
+        await Menu.appendContainer(dotNetHelper, container);
+        await Menu.alignOverlay(dotNetHelper, commponentId);
+        Menu.bindOutsideClickListener(dotNetHelper, commponentId);
+        Menu.bindResizeListener(dotNetHelper, commponentId);
+
+        if (await dotNetHelper.invokeMethodAsync("getAutoZIndex")) {
+            container.style.zIndex = String(await dotNetHelper.invokeMethodAsync("getBaseZIndex") + window.DomHandler.generateZIndex());
+        }
+    }
+    static onLeave(commponentId) {
+        Menu.unbindOutsideClickListener(commponentId);
+        Menu.unbindResizeListener(commponentId);
+    }
+    static async alignOverlay(dotNetHelper, commponentId) {
+        let container = await dotNetHelper.invokeMethodAsync("getContainer");
+
+        let { target } = Menu.targetArray.find(m => m.id === commponentId);
+
+        window.DomHandler.absolutePosition(container, target);
+
+        if (window.DomHandler.getOffset(container).top < DomHandler.getOffset(target).top) {
+            window.DomHandler.addClass(container, 'p-overlaypanel-flipped');
+        }
+    }
+    static bindOutsideClickListener(dotNetHelper, commponentId) {
+        if (!Menu.outsideClickListener.some(m => m.id === commponentId)) {
+            let outsideClick = async (event) => {
+                let container = await dotNetHelper.invokeMethodAsync("getContainer");
+                if (await dotNetHelper.invokeMethodAsync("getVisible") && container && !container.contains(event.target) && !Menu.isTargetClicked(event, commponentId)) {
+                    await dotNetHelper.invokeMethodAsync("setVisible", false)
+                }
+            };
+            document.addEventListener('click', outsideClick);
+            Menu.outsideClickListener.push({ id: commponentId, click: outsideClick })
+        }
+    }
+    static unbindOutsideClickListener(commponentId) {
+        if (Menu.outsideClickListener.some(m => m.id == commponentId)) {
+            let { click } = Menu.outsideClickListener.find(m => m.id === commponentId);
+            document.removeEventListener('click', click);
+            Menu.outsideClickListener = Menu.outsideClickListener.filter(m => m.id !== commponentId)
+        }
+    }
+    static bindResizeListener(dotNetHelper, commponentId) {
+        if (!Menu.resizeListener.some(m => m.id == commponentId)) {
+            let resizeListener = async () => {
+                if (await dotNetHelper.invokeMethodAsync("getVisible")) {
+                    await dotNetHelper.invokeMethodAsync("setVisible", false)
+                }
+            };
+            window.addEventListener('resize', resizeListener);
+            Menu.resizeListener.push({ id: commponentId, resize: resizeListener });
+        }
+    }
+    static unbindResizeListener(commponentId) {
+        if (Menu.resizeListener.some(m => m.id == commponentId)) {
+            let { resize } = Menu.resizeListener.find(m => m.id === commponentId);
+            window.removeEventListener('resize', resize);
+            Menu.resizeListener = Menu.resizeListener.filter(m => m.id !== commponentId);
+        }
+    }
+    static isTargetClicked(event, commponentId) {
+        let { target } = Menu.targetArray.find(m => m.id === commponentId);
+        return target && (target === event.target || target.contains(event.target));
+    }
+    static async appendContainer(dotNetHelper, container) {
+        let appendTo = await dotNetHelper.invokeMethodAsync("getVisible");
+        if (appendTo) {
+            if (appendTo === 'body')
+                document.body.appendChild(container);
+            else
+                document.getElementById(appendTo).appendChild(container);
+        }
+    }
+    static async restoreAppend(commponentId) {
+        let { dotNetHelper } = Menu.dotNetHelperArray.find(m => m.id === commponentId);
+        let container = await dotNetHelper.invokeMethodAsync("getContainer")
+        let appendTo = await dotNetHelper.invokeMethodAsync("getVisible");
+        if (container && appendTo) {
+            if (appendTo === 'body')
+                document.body.removeChild(container);
+            else
+                document.getElementById(appendTo).removeChild(container);
+        }
+    }
+}
+window.Menu = Menu;
