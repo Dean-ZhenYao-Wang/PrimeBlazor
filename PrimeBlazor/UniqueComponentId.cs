8ce47bddd6b7ed1ad02c7b252b203f34171ed4f4
王振耀 <wsbp2019@163.com>
2024-01-17 17:06:46 +0800

    删除datatable

---
+++ PrimeBlazor/UniqueComponentId.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeBlazor
+{
+    public static class UniqueComponentId
+    {
+        private static int lastId = 0;
+        public static string ComponentId(string prefix = "pv_id_")
+        {
+            lastId++;
+            return $"{prefix}{lastId}";
+        }
+    }
+}
